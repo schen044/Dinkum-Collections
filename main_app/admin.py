d6ca297625f883a59e606cd818bceb27e321e763
schen044 <ninjarulers@yahoo.com>
2022-10-26 18:58:39 -0700

    Adds Museum model for one-to-many relationship

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # import your models here
-from .models import Fish
+from .models import Fish, Museum
 
 # Register your models here
 admin.site.register(Fish)
+admin.site.register(Museum)
--- main_app/models.py
+++ main_app/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class Museum(models.Model):
+    town_name = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.town_name}'s Museum"
+        
 class Fish(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     time_active = models.CharField(max_length=100)
     season = models.CharField(max_length=100)
     price = models.IntegerField()
+    museum = models.ForeignKey(Museum, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
